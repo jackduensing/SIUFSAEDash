a18e0dd7f6977411625225a016e1d0fc8bd5e333
jackduensing <jack.duensing@gmail.com>
2026-03-12 21:20:29 -0500

    wrote lora process

--- loraDash.py
+++ loraDash.py
@@ -1,6 +1,11 @@
 import os
 from multiprocessing import shared_memory
 import numpy as np
+import board
+import busio
+import digitalio
+import adafruit_rfm9x
+import time
 
 def run(mem_name, type, lock):
     print(f"From process lora, recieved {mem_name}")
@@ -10,6 +15,28 @@ def run(mem_name, type, lock):
 
     #creates an array that mirrors the shared memory
     data = np.ndarray(shape=(1,), dtype=type, buffer=shared_container.buf)
+
+    cs = digitalio.DigitalInOut(board.D18)
+    reset = digitalio.DigitalInOut(board.D25)
+    freq = 915.0
+
+    spi = busio.SPI(clock=board.D21, MOSI=board.D20 , MISO=board.D19)
+
+    lora = adafruit_rfm9x.RFM9x(spi, cs, reset, freq)
+
+    while True:
+
+        time.sleep(1)
+
+        with lock:
+            try:
+                lora.send(data)
+            except Exception as e:
+                with open("log.txt", "a") as file:
+                    print(f"{e}\n", file=file)
+                break
+
+    
 
     shared_container.close()
     shared_container.unlink()
