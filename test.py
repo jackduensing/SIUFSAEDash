1492d2c818272129947acf2b70709f247fc76ff3
jackduensing <jack.duensing@gmail.com>
2026-03-24 21:09:12 -0500

    refactored test to just create the canframes

--- test.py
+++ test.py
@@ -8,22 +8,15 @@ fields = ("runtime", 'rpm', 'clt', 'map', 'mat', 'tps', 'adv_deg', 'afttgt1', 'A
 
 db = cantools.database.load_file("MS3.dbc")
 
-bus = can.Bus(channel="can0", interface="socketcan", bitrate=500000)
+msg_obj = db.get_message_by_name("megasquirt_dash0")
 
-def send_msg(field, value):
+data = msg_obj.encode({
+    "rpm": 5000,
+    "clt": 225,
+    "map": 0,
+    "tps": 0
+})
 
-    if field not in fields:
-        print("invalid field")
+message = can.Message(arbitration_id=msg_obj.frame_id, data=data)
 
-    msg_obj = db.get_message_by_name("test_msg")
-
-    data = msg_obj.encode({field:value})
-
-    message = can.Message(arbitration_id=msg_obj.frame_id, data=data)
-
-    bus.send(message)
-
-
-send_msg('rpm', 5000)
-time.sleep(5)
-send_msg('clt', 225)
+print(message)
