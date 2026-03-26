43ba0b4f4b6082a1cbfb93d61fdb8155e572eb81
jackduensing <jack.duensing@gmail.com>
2026-03-25 20:13:49 -0500

    debug

--- canDash.py
+++ canDash.py
@@ -33,8 +33,8 @@ def run(mem_name, type, lock):
             else:
                 print(f"recieved message {message}")
                 decoded_data = db.decode_message(message.arbitration_id, message.data)
-                type = type(decoded_data)
-                print(f"decoded data: {decoded_data} of type {type}")
+                data_type = type(decoded_data)
+                print(f"decoded data: {decoded_data} of type {data_type}")
 
                 for key, value in decoded_data:
                     print(f"{key}:{value}")
