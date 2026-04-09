f952b835765468ec0ab07d1665042cff6116b3d5
jackduensing <jack.duensing@gmail.com>
2026-04-09 18:50:39 -0500

    more specific rpm bar

--- test.py
+++ test.py
@@ -11,21 +11,25 @@ db = cantools.database.load_file("MS3.dbc")
 
 msg_obj = db.get_message_by_name("megasquirt_gp0")
 
-data = msg_obj.encode({
-    "rpm": 3500,
+for n in range(0, 12500, 500):
+    data = msg_obj.encode({
+    "rpm": n,
     "pw2": 0,
     "pw1": 0,
-    "seconds": 25
-})
+    "seconds": n/100
+    })
 
-message = can.Message(arbitration_id=msg_obj.frame_id, data=data)
+    message = can.Message(arbitration_id=msg_obj.frame_id, data=data)
 
-data_hex = ''.join(f'{b:02X}' for b in data)
-id_hex = f'{message.arbitration_id:03X}'
+    data_hex = ''.join(f'{b:02X}' for b in data)
+    id_hex = f'{message.arbitration_id:03X}'
 
-command  = "cansend can0 " + id_hex + "#" + data_hex
+    command  = "cansend can0 " + id_hex + "#" + data_hex
 
-print(command)
+    print("sleep 1")
+    print(command)
+
+
 
 msg_obj = db.get_message_by_name("megasquirt_gp3")
 
@@ -43,6 +47,6 @@ id_hex = f'{message.arbitration_id:03X}'
 
 command  = "cansend can0 " + id_hex + "#" + data_hex
 
-print(command)
+#print(command)
 
 
