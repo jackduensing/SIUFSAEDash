f828240e8c093fa25326a05ecf2ff12399ed0775
jackduensing <jack.duensing@gmail.com>
2026-04-22 20:09:10 -0500

    updated test for more data sending

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ import time
 import can
 import os
 import cantools
+import re
+        
 
 fields = ("runtime", 'rpm', 'clt', 'map', 'mat', 'tps', 'adv_deg', 'afttgt1', 'AFR1', 'batt')
 
 db = cantools.database.load_file("MS3.dbc")
-
-msg_obj = db.get_message_by_name("megasquirt_gp0")
 
 print(f"#!/bin/bash")
 
@@ -17,6 +17,10 @@ print("echo Starting test...")
 
 print("while true; do")
 
+##################################################################################
+
+
+msg_obj = db.get_message_by_name("megasquirt_gp0")
 for n in range(0, 12500, 500):
     data = msg_obj.encode({
     "rpm": n,
@@ -53,26 +57,49 @@ for n in range(12500, -500, -500):
     print("sleep 0.01")
     print(command)
 
+
+msg_obj = db.get_message_by_name("megasquirt_gp2")
+for n in range(0, 250, 25):
+    data = msg_obj.encode({
+    "clt": n,
+    "mat": 0,
+    "map": 0,
+    "baro": 0
+    })
+
+    message = can.Message(arbitration_id=msg_obj.frame_id, data=data)
+
+    data_hex = ''.join(f'{b:02X}' for b in data)
+    id_hex = f'{message.arbitration_id:03X}'
+
+    command  = "cansend can0 " + id_hex + "#" + data_hex
+
+    print("sleep 0.01")
+    print(command)
+
+for n in range(250, 0, -25):
+    data = msg_obj.encode({
+    "clt": n,
+    "mat": n/10,
+    "map": n/10,
+    "baro": 0
+    })
+
+    message = can.Message(arbitration_id=msg_obj.frame_id, data=data)
+
+    data_hex = ''.join(f'{b:02X}' for b in data)
+    id_hex = f'{message.arbitration_id:03X}'
+
+    command  = "cansend can0 " + id_hex + "#" + data_hex
+
+    print("sleep 0.01")
+    print(command)
+    
+
+
+##################################################################################
+
+
 print("done")
-
-
-
-msg_obj = db.get_message_by_name("megasquirt_gp3")
-
-data = msg_obj.encode({
-    "afr2_old": 0,
-    "afr1_old": 0,
-    "batt": 14.5,
-    "tps": 0
-})
-
-message = can.Message(arbitration_id=msg_obj.frame_id, data=data)
-
-data_hex = ''.join(f'{b:02X}' for b in data)
-id_hex = f'{message.arbitration_id:03X}'
-
-command  = "cansend can0 " + id_hex + "#" + data_hex
-
-#print(command)
 
 
